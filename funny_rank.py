c8bb0865b0aa1f1ec444cea74fd7248f05c8da6c
BrianGaillard <b.gaillard647@gmail.com>
2021-06-18 18:38:02 -0400

    added ranking method

--- funny_rank.py
+++ funny_rank.py
@@ -9,13 +9,16 @@ def FunnyRankings():
 
     f.close()
 
-    users = []
+    ranks = []
 
-    for i in range(len(data)):
-        users.append(data[i]['user'])
+    for user in range(len(data)):
+        funnyval = 0
+        for convo in range(len(data[user]['conversations'])):
+            for sens in range(len(data[user]['conversations'][convo])):
+                for sen in range(len(data[user]['conversations'][convo]['sentences'])):
+                    if data[user]['conversations'][convo]['sentences'][sen]['speaker_type'] == 'sales_rep':
+                        if data[user]['conversations'][convo]['sentences'][sen]['funny'] == True:
+                            funnyval = funnyval + 1
+        ranks.append([data[user]['user'], funnyval])
 
-
-    # for user in data:
-    #     for converstaions in users:
-
-    return users
+    return ranks
