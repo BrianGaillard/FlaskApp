eeed89dd2ac3999973751425f2f89dcd697fc4da
BrianGaillard <b.gaillard647@gmail.com>
2021-06-18 21:30:48 -0400

    added some style changes

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from wtforms import SubmitField
 from funny_rank import FunnyRankings
 
 
-
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'b2eb70701e05d178d1683604a5ca1ba9'
@@ -13,21 +12,19 @@ app.config['SECRET_KEY'] = 'b2eb70701e05d178d1683604a5ca1ba9'
 def homepage():
     rankings = FunnyRankings()
     length = len(rankings)
-
     check = True
 
     if request.method == 'POST':
         if request.form.get("submit_button") == 'High to Low':
-
                 rankings.sort(reverse=True,key= lambda ranks : ranks[1])
 
                 return render_template("layout.html", rankings = rankings, length = length, check = not(check))
 
         else:
-
                 rankings.sort(reverse=False,key= lambda ranks : ranks[1])
 
                 return render_template("layout.html", rankings = rankings, length = length, check = not(check))
+
     return render_template("layout.html", rankings = rankings, length = length, check = not(check))
 
 
