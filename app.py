9f5e8555e3a4c53a525acbf87e43b495fcec2115
BrianGaillard <b.gaillard647@gmail.com>
2021-06-18 21:35:48 -0400

    updated comments

--- app.py
+++ app.py
@@ -8,24 +8,25 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'b2eb70701e05d178d1683604a5ca1ba9'
 
+
 @app.route("/", methods=['GET', 'POST'])
 def homepage():
     rankings = FunnyRankings()
     length = len(rankings)
-    check = True
 
+    # use two diffferent buttons to toggle between sorting methods
     if request.method == 'POST':
         if request.form.get("submit_button") == 'High to Low':
                 rankings.sort(reverse=True,key= lambda ranks : ranks[1])
 
-                return render_template("layout.html", rankings = rankings, length = length, check = not(check))
+                return render_template("layout.html", rankings = rankings, length = length)
 
         else:
                 rankings.sort(reverse=False,key= lambda ranks : ranks[1])
 
-                return render_template("layout.html", rankings = rankings, length = length, check = not(check))
+                return render_template("layout.html", rankings = rankings, length = length)
 
-    return render_template("layout.html", rankings = rankings, length = length, check = not(check))
+    return render_template("layout.html", rankings = rankings, length = length)
 
 
 if __name__ == '__main__':
--- funny_rank.py
+++ funny_rank.py
@@ -3,6 +3,7 @@ import json
 
 
 def FunnyRankings():
+    # read in data and store data
     f = open('data.json')
 
     data = json.load(f)
@@ -11,6 +12,8 @@ def FunnyRankings():
 
     ranks = []
 
+    # iterate through dictionary, save the names of users as well as how
+    # many times they said something funny
     for user in range(len(data)):
         funnyval = 0
         for convo in range(len(data[user]['conversations'])):
@@ -21,6 +24,7 @@ def FunnyRankings():
                             funnyval = funnyval + 1
         ranks.append([data[user]['user'], funnyval])
 
+    # sort the list
     ranks.sort(key= lambda ranks : ranks[1])
 
     return ranks
